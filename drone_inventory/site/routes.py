cbf96c126f5f8144bb4157c21ad0405722326221
Joel Carter <carter3689@gmail.com>
2021-04-11 21:26:47 -0500

    Adding more templates for profile - and CSS for profile

--- drone_inventory/site/routes.py
+++ drone_inventory/site/routes.py
@@ -15,4 +15,9 @@ site = Blueprint('site', __name__, template_folder='site_templates')
 @site.route('/')
 def home():
     return render_template('index.html')
+    
+
+@site.route('/profile')
+def profile():
+    return render_template('profile.html')
 
