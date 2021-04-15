58d8f019d30e84c5457e0476516c421aebcb2626
Joel Carter <carter3689@gmail.com>
2021-04-15 01:12:22 -0500

    Adding new DB URL

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ class Config():
         create the config variable if not done already.
     """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off update messages from the sqlalchemy
